7811e75c981e546f676b060629e6179e72228c36
Afgan Rasulov <5529999+afganrasulov@users.noreply.github.com>
2025-12-18 16:02:03 +0300

    feat: Export new bulk operation functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // app.js - Main Application Entry Point
 import { initAuth, logout, addUser, removeUser } from './auth.js';
 import { loadPosts, setFilter, publishPost, unpublishPost, deletePost, editPost, savePost } from './posts.js';
-import { loadVideos, fetchAndSaveVideos, openVideoModal, loadMoreVideos } from './videos.js';
+import { loadVideos, fetchAndSaveVideos, openVideoModal, loadMoreVideos, toggleVideoSelection, toggleSelectAll, clearSelection, setVideoFilter, bulkTranscribe, bulkGenerateBlog } from './videos.js';
 import { transcribeAll, startBackgroundTranscription, generateBlog, saveBlog } from './transcription.js';
 import { saveSettings, toggleAutopilot, onProviderChange } from './settings.js';
 import { switchPage, openModal, closeModal } from './utils.js';
@@ -25,6 +25,12 @@ window.app = {
   fetchAndSaveVideos,
   openVideoModal,
   loadMoreVideos,
+  toggleVideoSelection,
+  toggleSelectAll,
+  clearSelection,
+  setVideoFilter,
+  bulkTranscribe,
+  bulkGenerateBlog,
   
   // Transcription
   transcribeAll,
