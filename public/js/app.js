c1c3091d50edc5c4a09694c85586f66087a45624
Afgan Rasulov <5529999+afganrasulov@users.noreply.github.com>
2025-12-18 15:51:14 +0300

    feat: Export loadMoreVideos function

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // app.js - Main Application Entry Point
 import { initAuth, logout, addUser, removeUser } from './auth.js';
 import { loadPosts, setFilter, publishPost, unpublishPost, deletePost, editPost, savePost } from './posts.js';
-import { loadVideos, fetchAndSaveVideos, openVideoModal } from './videos.js';
+import { loadVideos, fetchAndSaveVideos, openVideoModal, loadMoreVideos } from './videos.js';
 import { transcribeAll, startBackgroundTranscription, generateBlog, saveBlog } from './transcription.js';
 import { saveSettings, toggleAutopilot, onProviderChange } from './settings.js';
 import { switchPage, openModal, closeModal } from './utils.js';
@@ -24,6 +24,7 @@ window.app = {
   // Videos
   fetchAndSaveVideos,
   openVideoModal,
+  loadMoreVideos,
   
   // Transcription
   transcribeAll,
